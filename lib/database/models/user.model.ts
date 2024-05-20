0456445b299cdd06cfb05e7a1c98709d075e902b
GauravSinghSamant <thorusamant8@gmail.com>
2024-05-20 15:26:05 +0530

    Changes in UI

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -2,11 +2,11 @@ import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true, default:'' },
+  email: { type: String, required: false, default:'' },
   username: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true, default: '' },
-  lastName: { type: String, required: true, default: '' },
-  photo: { type: String, required: true, default:' ' },
+  firstName: { type: String, required: false, default: '' },
+  lastName: { type: String, required: false, default: '' },
+  photo: { type: String, required: false, default:' ' },
 });
 
 const User = models.User || model('User', UserSchema);
